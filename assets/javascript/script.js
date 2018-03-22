f6798c467ecf4a9be5ce1802d422ebd9e42d5673
Nasteho <Nasteho@users.noreply.github.com>
2018-03-22 21:39:57 +0100

    material tabs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,45 @@
-function showKEDUmATERIAL() {
+/*var kedumaterial = document.getElementById('kedumaterial');
+var mymaterial = document.getElementById('mymaterial');
+var communitymaterial = document.getElementById('communitymaterial');
+
+function showKeduMaterial() {
+  kedumaterial.style.display = 'block';
+  communitymaterial.style.display = 'none';
+  mymaterial.style.display = 'none';
+  }
+}
+
+function showMyMaterial() {
+  kedumaterial.style.display = 'none';
+  communitymaterial.style.display = 'none';
+  mymaterial.style.display = 'block';
+}
+
+function showCommunityMaterial() {
+  kedumaterial.style.display = 'none'
+  communitymaterial.style.display = 'block';
+  mymaterial.style.display = 'none';
+
+}
+*/
+function showMyMaterial() {
+    var mymaterial = document.getElementById("mymaterial");
+    kedumaterial.style.display = 'none';
+    communitymaterial.style.display = 'none';
+    mymaterial.style.display = 'block';
 
 }
 
-function showKEDUmATERIAL() {
-
+function showKeduMaterial() {
+    var mymaterial = document.getElementById("mymaterial");
+    kedumaterial.style.display = 'block';
+    communitymaterial.style.display = 'none';
+    mymaterial.style.display = 'none';
 }
 
-function showKEDUmATERIAL() {
-
+function showCommunityMaterial() {
+    var communitymaterial = document.getElementById("communitymaterial");
+    kedumaterial.style.display = 'none';
+    communitymaterial.style.display = 'block';
+    mymaterial.style.display = 'none';
 }
