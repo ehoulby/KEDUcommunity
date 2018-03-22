f49db400b92cd7e43cb1d9007df29e31c6a655f4
Nasteho <Nasteho@users.noreply.github.com>
2018-03-22 21:51:25 +0100

    d

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,6 @@
-/*var kedumaterial = document.getElementById('kedumaterial');
-var mymaterial = document.getElementById('mymaterial');
-var communitymaterial = document.getElementById('communitymaterial');
-
-function showKeduMaterial() {
-  kedumaterial.style.display = 'block';
-  communitymaterial.style.display = 'none';
-  mymaterial.style.display = 'none';
-  }
-}
-
-function showMyMaterial() {
-  kedumaterial.style.display = 'none';
-  communitymaterial.style.display = 'none';
-  mymaterial.style.display = 'block';
-}
-
-function showCommunityMaterial() {
-  kedumaterial.style.display = 'none'
-  communitymaterial.style.display = 'block';
-  mymaterial.style.display = 'none';
-
-}
-*/
 function showMyMaterial() {
     var mymaterial = document.getElementById("mymaterial");
+
     kedumaterial.style.display = 'none';
     communitymaterial.style.display = 'none';
     mymaterial.style.display = 'block';
@@ -31,7 +8,7 @@ function showMyMaterial() {
 }
 
 function showKeduMaterial() {
-    var mymaterial = document.getElementById("mymaterial");
+    var kedumaterial = document.getElementById("kedumaterial");
     kedumaterial.style.display = 'block';
     communitymaterial.style.display = 'none';
     mymaterial.style.display = 'none';
