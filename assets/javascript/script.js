e40e33b4541ea2e7cb8f87f64fd11faad2171e37
Nasteho <Nasteho@users.noreply.github.com>
2018-03-22 20:11:05 +0100

    cc

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,11 @@
-<script>
-function showMaterial(evt, materialTab) {
-    var i, tabcontent, tablinks;
+function showKEDUmATERIAL() {
 
-materialtabs = document.getElementsByClassName("materialcontent");
-for (i = 0; i < materialtabs.length; i++) {
-    materialtabs[i].style.display = "none";
 }
-materialtabbuttons = document.getElementsByClassName("materialtabbuttons");
-for (i = 0; i < tablinks.length; i++) {
-    materialtabbuttons[i].className = materialtabbuttons[i].className.replace(" active", "");
+
+function showKEDUmATERIAL() {
+
 }
-document.getElementById(materialTab).style.display = "block";
-evt.currentTarget.className += " active";
+
+function showKEDUmATERIAL() {
+
 }
-</script>
